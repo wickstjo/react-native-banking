8c3a91b345cc11fc87e1c9d095dbb34f2ebb93bf
wickstjo <33825194+wickstjo@users.noreply.github.com>
2019-04-03 11:47:51 +0300

    Added direction finder

--- components/shared/map.js
+++ components/shared/map.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 
 class Map extends Component {
 
@@ -12,6 +14,7 @@ class Map extends Component {
       }
    }
 
+   // PRIMARY WAYPOINT
    primary = () => {
       if (this.props.primary.coords !== undefined) {
          return <Marker
@@ -22,6 +25,7 @@ class Map extends Component {
       }
    }
 
+   // SECONDARY WAYPOINTS
    secondary = () => {
       return this.props.waypoints.map((item, index) =>
          <Marker
@@ -33,14 +37,26 @@ class Map extends Component {
       )
    }
 
+   // DRAW DIRECTIONAL POLYLINE
+   directions = () => {
+      let wps = [];
+      this.props.waypoints.map(item => wps.push(item.coords))
+
+      return <MapViewDirections
+         origin={ this.props.primary.coords }
+         destination={ this.props.primary.coords }
+         waypoints={ wps }
+         apikey={ 'AIzaSyBcbkfxWDiiWg6sjnkWHdsrsW7-bT7tfE8' }
+         strokeWidth={ 3 }
+         strokeColor={ 'hotpink' }
+      />
+   }
+
    render() { return (
-      <MapView
-         provider={ PROVIDER_GOOGLE }
-         style={ styles.container }
-         initialRegion={ this.state.region }
-      >
+      <MapView provider={ PROVIDER_GOOGLE } style={ styles.container } initialRegion={ this.state.region }>
          { this.primary() }
          { this.secondary() }
+         { this.directions() }
       </MapView>
    )}
 }
